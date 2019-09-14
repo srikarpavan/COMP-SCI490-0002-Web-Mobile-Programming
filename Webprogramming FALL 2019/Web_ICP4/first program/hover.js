a7c0b785d1704208d8550bec05c373112e6535d2
srikarpavan <47261789+srikarpavan@users.noreply.github.com>
2019-09-14 15:48:36 -0500

    commit

---
+++ Webprogramming FALL 2019/Web_ICP4/first program/hover.js
@@ -0,0 +1,28 @@
+function upDate(txtimg) {
+    /* In this function you should
+       1) change the url for the background image of the div with the id = "image"
+       to the source file of the preview image
+
+       2) Change the text  of the div with the id = "image"
+       to the alt text of the preview image
+       */
+    var src = txtimg.getAttribute( "src" );
+    var alt = txtimg.getAttribute( "alt" );
+    document.getElementById('image').style.backgroundImage = "url('" + src + "')";
+    document.getElementById('image').innerHTML = alt;
+
+}
+
+function unDo() {
+    /* In this function you should
+   1) Update the url for the background image of the div with the id = "image"
+   back to the orginal-image.  You can use the css code to see what that original URL was
+
+   2) Change the text  of the div with the id = "image"
+   back to the original text.  You can use the html code to see what that original text was
+   */
+    X = document.getElementById('image');
+    X.style.backgroundImage = "url('')";
+    document.getElementById('image').innerHTML = "Hover over an image below to display here";
+
+}
